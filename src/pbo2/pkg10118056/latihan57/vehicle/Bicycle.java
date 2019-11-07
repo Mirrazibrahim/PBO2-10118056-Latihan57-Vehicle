f1a48824a10e4309e9d5f8881741b604fd76d959
Mirraz Ibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2019-11-07 23:12:22 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118056/latihan57/vehicle/Bicycle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan57.vehicle;
+
+/**
+ *
+ * @author 
+ * Nama : Mirraz Ibrahim
+ * Nim : 10118056
+ * Kelas: IF-2
+ */
+public class Bicycle extends Vehicle{
+    private int myGearCount;
+
+    public Bicycle() {
+        System.out.println("Bicycle");
+    }
+
+    public int getMyGearCount() {
+        return myGearCount;
+    }
+
+    public void setMyGearCount(int myGearCount) {
+        this.myGearCount = myGearCount;
+    }
+    
+    
+}
---
+++ src/pbo2/pkg10118056/latihan57/vehicle/Skateboard.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118056.latihan57.vehicle;
+
+/**
+ *
+ * @author 
+ * Nama : Mirraz Ibrahim
+ * Nim : 10118056
+ * Kelas: IF-2
+ */
+public class Skateboard extends Vehicle {
+    private double myBoarLenght;
+
+    public Skateboard() {
+        System.out.println("Skateboard");
+    }
+
+    public double getMyBoarLenght() {
+        return myBoarLenght;
+    }
+
+    public void setMyBoarLenght(double myBoarLenght) {
+        this.myBoarLenght = myBoarLenght;
+    }
+    
+    
+}
